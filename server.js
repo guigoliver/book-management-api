69ea9b43dd318ad02eeca4d66342aac97fb49d3d
guigoliver <profguilherme.as@gmail.com>
2024-02-26 19:47:22 -0300

    feat: initial crud operations

---
+++ server.js
@@ -0,0 +1,8 @@
+// import http from 'http';
+import app from "./src/app.js";
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('servidor escutando!')
+});
---
+++ src/app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos Anéis",
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit",
+    },
+]
+
+function buscaLivro (id) {
+    return livros.findIndex(livro => {
+        return livro.id === +id
+    })
+}
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Node.js")
+})
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id)
+    res.status(200).json(livros[index])
+})
+
+app.post("/livros", (req, res) => {
+    const livro = req.body
+    livros.push(livro)
+    res.status(201).send("Livro cadastrado com sucesso!")
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id)
+    livros[index].titulo = req.body.titulo
+    res.status(200).json(livros[index])
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id)
+    livros.splice(index, 1)
+    res.status(204).send("Livro excluído com sucesso!")
+})
+
+export default app
