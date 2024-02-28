4c672e0e810e63ddfc96815d9a9aaa74a06dfdff
guigoliver <profguilherme.as@gmail.com>
2024-02-28 16:25:19 -0300

    feat: move connection string to dotenv

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import conectaDatabase from './config/dbConnect.js'
+import livro from './models/Livro.js'
 
 const conexao = await conectaDatabase()
 
@@ -14,34 +15,19 @@ conexao.once("open", () => {
 const app = express()
 app.use(express.json())
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis",
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit",
-    },
-]
-
-function buscaLivro (id) {
-    return livros.findIndex(livro => {
-        return livro.id === +id
-    })
-}
-
-app.get("/", (req, res) => {
+// ROUTES
+app.get("/", (req, res) => {    
     res.status(200).send("Curso de Node.js")
 })
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros)
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({})
+    res.status(200).json(listaLivros)
 })
 
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id)
-    res.status(200).json(livros[index])
+app.get("/livros/:id", async (req, res) => {
+    const livro = await livro.findById(req.params.id)
+    res.status(200).json(livro)
 })
 
 app.post("/livros", (req, res) => {
