1bbf5f132800fffd31de86a80ccd2ff955547f5f
guigoliver <profguilherme.as@gmail.com>
2024-03-14 07:45:46 -0300

    feat: add mongoose validation on books

--- src/models/Book.js
+++ src/models/Book.js
@@ -5,7 +5,10 @@ const livroSchema = new mongoose.Schema({
   title: { type: String, required: [true, 'Title is required'] },
   publisher: { type: String, required: [true, 'Publisher is required'] },
   cost: { type: Number },
-  pages: { type: Number },
+  pages: { type: Number,
+    min: [10, 'Book must have at least 10 pages'], 
+    max: [5000, 'Book must have at most 5000 pages'] 
+  },
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'authors', required: [true, 'Author is required']},
 }, { versionKey: false })
 
