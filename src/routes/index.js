b9d6f21cae52986dfdd3431e1b1ec228cf734fea
guigoliver <profguilherme.as@gmail.com>
2024-03-19 10:10:24 -0300

    style: changes on index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import books from './booksRoutes.js'
 import authors from './authorsRoutes.js'
 
 const routes = (app) => {
-  app.route('/').get((req, res) => res.status(200).send('Node Course'))
+  app.route('/').get((req, res) => res.status(200).send('ReBooks API'))
 
   app.use(express.json(), books, authors)   
 }
